8cb0cc70eff986a13021a777ee39da49bebec08d
Ndiaye HANN <cawral@hotmail.fr>
2021-09-03 09:50:40 +0200

    Test commit

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\Openclassrooms\p2-v2\env"
+$env:VIRTUAL_ENV="D:\Openclassrooms\Projet2_V2\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Openclassrooms\p2-v2\env
+set VIRTUAL_ENV=D:\Openclassrooms\Projet2_V2\env
 
 if not defined PROMPT set PROMPT=$P$G
 
